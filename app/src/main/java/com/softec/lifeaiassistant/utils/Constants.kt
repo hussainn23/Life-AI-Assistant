753d176e689b7608f49dccf1726630285ff84307
hussainn23 <hussainnhussain023@gmail.com>
2025-04-26 12:55:53 +0500

    shared pref utils added

---
+++ app/src/main/java/com/softec/lifeaiassistant/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.softec.lifeaiassistant.utils
+
+object Constants {
+
+    ////////////////////////////////KEYS//////////////////////////////
+
+    const val PREFERENCE = "myAppPrefs"
+    const val TIMEOUT_DURATION = 30L
+    const val LOGIN = "login"
+}
